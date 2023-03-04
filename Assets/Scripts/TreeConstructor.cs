b1411dcfa9f9cbc2cffd9dc910bceeca17e68cf7
Thogan24 <thogan1924@gmail.com>
2023-03-04 14:00:39 -0500

    Added branch(), branch probability, and while loop for blocks

--- Assets/Scripts/TreeConstructor.cs
+++ Assets/Scripts/TreeConstructor.cs
@@ -5,6 +5,8 @@ public class TreeConstructor : MonoBehaviour
     public GameObject Block;
     public Vector3 sampleSpawnPoint = new Vector3(0, 0, 0);
     public Vector3 sampleSpawnRotation = new Vector3(0, 0, 0);
+    public int branchProbability = 20;
+    private int branchChance = 0;
     private void Update()
     {
 
@@ -17,15 +19,31 @@ public class TreeConstructor : MonoBehaviour
     void generateTree(Vector3 spawnPoint, Vector3 spawnRotation, int blockSize, int blockHeight, int blocksLeft)
     {
         Debug.Log("Generating Tree...");
-        GameObject firstBlock = Instantiate(Block, spawnPoint + new Vector3(0, blockHeight/2, 0), Quaternion.identity);
-        firstBlock.transform.localEulerAngles = new Vector3(0, 0, 0);
+        branchChance = 0;
 
-
-
-
+        while(blocksLeft > 0)
+        {
+            spawnPoint += new Vector3(0, blockHeight, 0);
+            GameObject currentBlock = Instantiate(Block, spawnPoint + new Vector3(0, blockHeight / 2, 0), Quaternion.identity);
+            currentBlock.transform.localEulerAngles = new Vector3(0, 0, 0);
+            branchChance += branchProbability;
+            blocksLeft--;
+            if (Random.Range(1, 101) <= branchChance)
+            {
+                Debug.Log("Branched");
+                branch(blocksLeft);
+                break;
+            }
+        }
+        
 
 
 
         Debug.Log("Tree Successfully Generated.");
+    }
+
+    void branch(int blocksLeft)
+    {
+        //generateTree(, , , ,blocksLeft / 2);
     }
 }
