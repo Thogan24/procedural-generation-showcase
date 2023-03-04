bdcc61d02d08144d30eff758fd1f9c5cd7d61e9d
Thogan24 <thogan1924@gmail.com>
2023-03-04 12:47:35 -0500

    Organization + Setting up

---
+++ Assets/Scripts/TreeConstructor.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TreeConstructor : MonoBehaviour
+{
+    public GameObject Block;
+    void generateTree(Transform spawnPoint, int size, int height, int blocksLeft)
+    {
+        
+    }
+}
