de469b0ee3f012ac7199639dc169767ef77477bd
Thogan24 <thogan1924@gmail.com>
2023-03-04 13:33:02 -0500

    Started coding generateTree() function

--- Assets/Scripts/TreeConstructor.cs
+++ Assets/Scripts/TreeConstructor.cs
@@ -3,8 +3,29 @@ using UnityEngine;
 public class TreeConstructor : MonoBehaviour
 {
     public GameObject Block;
-    void generateTree(Transform spawnPoint, int size, int height, int blocksLeft)
+    public Vector3 sampleSpawnPoint = new Vector3(0, 0, 0);
+    public Vector3 sampleSpawnRotation = new Vector3(0, 0, 0);
+    private void Update()
     {
-        
+
+        if (Input.GetKeyDown("e"))
+        {
+            generateTree(sampleSpawnPoint, sampleSpawnRotation, 1, 2, 20);
+        }
+    }
+
+    void generateTree(Vector3 spawnPoint, Vector3 spawnRotation, int blockSize, int blockHeight, int blocksLeft)
+    {
+        Debug.Log("Generating Tree...");
+        GameObject firstBlock = Instantiate(Block, spawnPoint + new Vector3(0, blockHeight/2, 0), Quaternion.identity);
+        firstBlock.transform.localEulerAngles = new Vector3(0, 0, 0);
+
+
+
+
+
+
+
+        Debug.Log("Tree Successfully Generated.");
     }
 }
