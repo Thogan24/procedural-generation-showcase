4ed5d40a77065417b8228d0f14fe65cdfe0d43a9
Thogan24 <thogan1924@gmail.com>
2023-07-14 08:57:17 -0400

    Updated movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,38 +2,43 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float moveSpeed = 50f;
-    private float shiftMultiplier;
-    public float scrollSpeed = 250f;
-    public float rotateSpeed = 150f;
+    [SerializeField] private float moveSpeed = 50f;
+    [SerializeField] private float scrollSpeed = 250f;
+    [SerializeField] private float rotateSpeed = 150f;
+    [SerializeField] private float shiftMultiplier = 3f;
+    [SerializeField] private float controlMultiplier = 0.3f;
+    [SerializeField] private float baseSpeed = 1f;
+    private float speedMultiplier;
+    
 
     void FixedUpdate()
     {
-
-        shiftMultiplier = Input.GetKey(KeyCode.LeftShift) ? 1.5f : 1f;
+        Debug.Log(Input.GetKey(KeyCode.LeftShift));
+        speedMultiplier = Input.GetKey(KeyCode.LeftShift) ? shiftMultiplier : baseSpeed;
+        speedMultiplier = Input.GetKey(KeyCode.LeftControl) ? controlMultiplier : speedMultiplier;
 
         if (Input.GetKey("w"))
         {
-            transform.Translate(Vector3.forward * moveSpeed * shiftMultiplier * Time.deltaTime / Time.timeScale);
+            transform.Translate(Vector3.forward * moveSpeed * speedMultiplier * Time.deltaTime / Time.timeScale);
         }
         if (Input.GetKey("s"))
         {
-            transform.Translate(Vector3.back * moveSpeed * shiftMultiplier * Time.deltaTime / Time.timeScale);
+            transform.Translate(Vector3.back * moveSpeed * speedMultiplier * Time.deltaTime / Time.timeScale);
         }
         if (Input.GetKey("a"))
         {
-            transform.Translate(Vector3.left * moveSpeed * shiftMultiplier * Time.deltaTime / Time.timeScale);
+            transform.Translate(Vector3.left * moveSpeed * speedMultiplier * Time.deltaTime / Time.timeScale);
         }
         if (Input.GetKey("d"))
         {
-            transform.Translate(Vector3.right * moveSpeed * shiftMultiplier * Time.deltaTime / Time.timeScale);
+            transform.Translate(Vector3.right * moveSpeed * speedMultiplier * Time.deltaTime / Time.timeScale);
         }
 
         GetComponent<Camera>().fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed * Time.deltaTime / Time.timeScale;
         GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView, 30, 90);
 
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
             //Clamp this
             transform.eulerAngles += rotateSpeed * new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime / Time.timeScale;
